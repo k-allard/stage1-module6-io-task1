700ccac93feec8b220e8ff0e18cedaa4f938a554
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-04-19 21:07:39 +0400

    Refactor code

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        Profile result = new Profile();
         StringBuilder sb = new StringBuilder();
         byte[] buffer = new byte[10];
         try (FileInputStream fis = new FileInputStream(file)) {
@@ -18,26 +17,31 @@ public class FileReader {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String[] profileContentArr = sb.toString().split("\n");
-        for (String profileString : profileContentArr) {
-            String[] words = profileString.split(": ");
+        return getProfileFromRawString(sb.toString());
+    }
+
+    private Profile getProfileFromRawString(String fileString) {
+        String[] strings = fileString.split("\n");
+        Profile profile = new Profile();
+        for (String str : strings) {
+            String[] words = str.split(": ");
             switch (words[0]) {
                 case "Name":
-                    result.setName(words[1]);
+                    profile.setName(words[1]);
                     break;
                 case "Age":
-                    result.setAge(Integer.parseInt(words[1]));
+                    profile.setAge(Integer.parseInt(words[1]));
                     break;
                 case "Email":
-                    result.setEmail(words[1]);
+                    profile.setEmail(words[1]);
                     break;
                 case "Phone":
-                    result.setPhone(Long.parseLong(words[1]));
+                    profile.setPhone(Long.parseLong(words[1]));
                     break;
                 default:
                     break;
             }
         }
-        return result;
+        return profile;
     }
 }
